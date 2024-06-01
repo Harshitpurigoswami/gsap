8c0628aa16c75aee7f6c93b1368fad96ac91c7dd
Harshit <hpurigoswami786@gmail.com>
2024-06-01 21:26:27 +0530

    add scroll trigger

--- main.js
+++ main.js
@@ -1,9 +1,56 @@
 gsap.from('#box',{
-    duration: 2,
-    delay: 2,
+    duration: 1,
+    delay: 1,
     scale: 0,
     backgroundColor: 'blue',
     borderRadius: '50%',
     // stagger:1,
-    
+    repeat:-1,
+    yoyo:true,
 });
+
+
+let tl = gsap.timeline();
+
+tl.from('nav h1',{
+    delay: 0.5,
+    duration : 0.8,
+    y:-20,
+    opacity: 0,
+});
+
+tl.from('nav li',{
+    delay: 0.5,
+    duration : 0.8,
+    y:-20,
+    stagger: 0.3,
+    opacity: 0,
+    ease: "bounce.inOut",
+    ease: "bounce.out",
+});
+
+gsap.from(".page-1 h1",{
+    scale:0,
+    duration:1,
+    delay: 0.5,
+    scrollTrigger: ".page-1 h1",
+})
+gsap.to(".page-2  h1",{ 
+    transform: "translateX(-57%)", 
+    scrollTrigger : {
+        trigger: '.page-2',  // This is name where we trigger the scroller
+        scroller: 'body',  // don't no wet 
+        markers: true, // This is markers for get the loaction of scroller trigger
+        start: "top 0%", // Start point of trigger the animation 
+        end: "top -100%",  // Ent point of trigger 
+        scrub: 2, // This animation depend on scroll movement 
+        // scrub: 5, // when give number value we can smooth 
+        pin: true, // this is stop a scroll and  complete a animation then continue 
+    }
+})
+gsap.from(".page-3 h1",{
+    scale:0,
+    duration:1,
+    delay: 0.5,
+    scrollTrigger: ".page-3 h1",
+})
